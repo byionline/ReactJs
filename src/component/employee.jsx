902ef2cec42b997c649233f42da8cde29121f0a8
RAJNISH <10815566+byionline@users.noreply.github.com>
2018-12-22 16:11:16 +0530

    initial commit

---
+++ src/component/employee.jsx
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import MUIDataTable from "mui-datatables";
+import Layout from "./layout";
+class Employee extends Component {
+  render() {
+    /* Normal columns */
+
+    /* const columns = [
+      "Name",
+      "Company",
+      "Department",
+      "Salary",
+      "City",
+      "State"
+    ]; */
+
+    /* Custom columns */
+
+    const columns = [
+      {
+        name: "Name",
+        options: {
+          filter: false,
+          sort: true
+        }
+      },
+      {
+        name: "Company",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "Department",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "Salary",
+        options: {
+          filter: true,
+          sort: false
+        }
+      },
+      {
+        name: "City",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "State",
+        options: {
+          filter: true,
+          sort: true
+        }
+      }
+    ];
+
+    const data = [
+      ["Joe James", "Test Corp", "IoT", "$25000", "Yonkers", "NY"],
+      ["John Walsh", "Test Corp", "IoT", "$25000", "Hartford", "CT"],
+      ["Bob Herm", "Test Corp", "IoT", "$25000", "Tampa", "FL"],
+      ["James Houston", "Test Corp", "IoT", "$25000", "Dallas", "TX"],
+
+      ["Micheal Houston", "ABX Corp", "Electronics", "$25000", "Dallas", "NY"],
+      ["Bob Houston", "ABX Corp", "AI", "$25000", "Yonkers", "CT"],
+      ["James Houston", "ABX Corp", "AI", "$25000", "Tampa", "FL"],
+      [
+        "Joe Houston",
+        "ABX Corp",
+        "Computer Science",
+        "$25000",
+        "Hartford",
+        "TX"
+      ],
+
+      ["Mohan Kumar", "ZXC Corp", "Electronics", "$25000", "Yonkers", "NY"],
+      [
+        "Sunil Thakur",
+        "ZXC Corp",
+        "Computer Science",
+        "$25000",
+        "Hartford",
+        "CT"
+      ],
+      ["Binay", "ZXC Corp", "AI", "$25000", "Dallas", "FL"],
+      ["Robert", "ZXC Corp", "IoT", "$25000", "Tampa", "TX"],
+
+      ["Jonny Walsh", "FRQ Corp", "IoT", "$15000", "Dallas", "NY"],
+      ["James Herm", "FRQ Corp", "AI", "$5000", "Tampa", "CT"],
+      ["Rose Marry", "FRQ Corp", "Computer Science", "$2000", "Hartford", "FL"],
+      ["Bob Houston", "FRQ Corp", "Electronics", "$5000", "Yonkers", "TX"]
+    ];
+
+    const options = {
+      filterType: "checkbox"
+    };
+    return (
+      <Layout>
+        <MUIDataTable
+          title={"Employee List"}
+          data={data}
+          columns={columns}
+          options={options}
+        />
+      </Layout>
+    );
+  }
+}
+
+export default Employee;
---
+++ src/component/menuItem.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/People";
+import PersonIcon from "@material-ui/icons/Person";
+const styles = theme => ({ toolbar: theme.mixins.toolbar });
+class MenuItem extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div className={classes.toolbar} /> <Divider />
+        <ListItem button component={Link} to="/">
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button component={Link} to="/customer">
+          <ListItemIcon>
+            <PeopleIcon />
+          </ListItemIcon>
+          <ListItemText primary="Customers" />
+        </ListItem>
+        <ListItem button component={Link} to="/employee">
+          <ListItemIcon>
+            <PersonIcon />
+          </ListItemIcon>
+          <ListItemText primary="Employee" />
+        </ListItem>
+      </div>
+    );
+  }
+}
+MenuItem.propTypes = { classes: PropTypes.object.isRequired };
+export default withStyles(styles)(MenuItem);
